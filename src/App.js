f51b5b7edb707ad2d400305dfdbc45264bdcfa9f
serclino <sercl.adam@email.cz>
2022-05-03 19:25:31 +0200

    successfully fetched the data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      Ahoj!
-    </div>
-  )
+  const [status, setStatus] = useState("it is working");
+
+  const fetchReddit = async () => {
+    try {
+      const response = await fetch("https://www.reddit.com/r/popular.json");
+      console.log(response);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        setStatus(jsonResponse.data.children[0].data.title);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const newStatus = fetchReddit();
+  }, []);
+
+  return <div>{status}</div>;
 }
 
 export default App;
