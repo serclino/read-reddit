923a3c11e7a056730904329c2b3e6996f9dd095e
serclino <sercl.adam@email.cz>
2022-05-12 14:24:48 +0200

    added searchReducer to the store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../features/postsSlice";
 import filterReducer from "../features/filterSlice";
 import subredditsReducer from "../features/subredditsSlice";
+import searchReducer from "../features/searchSlice";
 
 export default configureStore({
   reducer: {
     posts: postReducer,
     filter: filterReducer,
-    subreddits: subredditsReducer
+    subreddits: subredditsReducer,
+    search: searchReducer
   },
 });
