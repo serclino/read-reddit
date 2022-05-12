65f8463b03c508be6697cd1ad3527f06704ec80b
serclino <sercl.adam@email.cz>
2022-05-12 12:55:53 +0200

    SubredditHeader.js succesfully fetched the data from the store

--- src/components/subreddits/SubredditHeader.js
+++ src/components/subreddits/SubredditHeader.js
@@ -5,5 +5,20 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 export const SubredditHeader = () => {
-  return <div>SubredditHeader</div>;
+  const allSubreddits = useSelector(selectAllSubreddits);
+  const { subreddit } = useParams();
+  const mySubreddit = "r/" + subreddit;
+  const desiredProps = allSubreddits.find(item => item.subreddit === mySubreddit);
+  const { description, subscribers, title} = desiredProps;
+  const bannerImg = desiredProps.bannerImg;
+
+  return (
+      <>
+      <img src={bannerImg} alt="" />
+      <h1>{mySubreddit}</h1>
+      <h2>{title}</h2>
+      <h3>{description}</h3>
+      <h4>{subscribers}</h4>
+      </>
+  );
 };
