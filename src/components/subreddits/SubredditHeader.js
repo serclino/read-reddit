2ae2c08b5ffaf3ec44e567e9ee01cc18fb6db422
serclino <sercl.adam@email.cz>
2022-05-12 14:15:57 +0200

    SubredditPage.js is 100% complete

--- src/components/subreddits/SubredditHeader.js
+++ src/components/subreddits/SubredditHeader.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./subredditHeader.css";
 import { selectAllSubreddits } from "../../features/subredditsSlice";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
+import defaultBg from './images/default-bg.jpg';
+
 export const SubredditHeader = () => {
   const allSubreddits = useSelector(selectAllSubreddits);
   const { subreddit } = useParams();
   const mySubreddit = "r/" + subreddit;
-  const desiredProps = allSubreddits.find(item => item.subreddit === mySubreddit);
-  const { description, subscribers, title} = desiredProps;
-  const bannerImg = desiredProps.bannerImg;
+  const desiredProps = allSubreddits.find(
+    (item) => item.subreddit === mySubreddit
+  );
+  const { description, subscribers, title } = desiredProps;
+  let bannerImg = desiredProps.bannerImg;
+  if (!bannerImg) {
+      bannerImg = defaultBg;
+  }
+
+  useEffect(() => {
+    let element = document.querySelector(".subreddit-header");
+    element.style.backgroundImage = `url(${bannerImg})`;
+  }, [title]);
 
   return (
-      <>
-      <img src={bannerImg} alt="" />
-      <h1>{mySubreddit}</h1>
-      <h2>{title}</h2>
-      <h3>{description}</h3>
-      <h4>{subscribers}</h4>
-      </>
+    <>
+      <div className="subreddit-header">
+        <h1>{title}</h1>
+        <p className="mySubreddit">{mySubreddit}</p>
+        <p className="subscribers">{subscribers} subscribers</p>
+      </div>
+      <div className="description">
+        <h2>About community:</h2>
+        <p>{description}</p>
+      </div>
+    </>
   );
 };
