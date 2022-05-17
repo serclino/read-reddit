de161fc8757dc1c3de9a4e551b2ec2d66d23b304
serclino <sercl.adam@email.cz>
2022-05-17 15:13:23 +0200

    styled ErrorPage.js

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className='error'>
+      <p>Oops! Something went wrong.</p>
+    </div>
+  );
 }
